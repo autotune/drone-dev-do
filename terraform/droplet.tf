b15f8821149e543eb0d1e5cd721e6c67ab8f9172
Brian Adams <service@contrasting.org>
2023-04-25 11:49:14 -0500

    feat: added do pub key

--- terraform/droplet.tf
+++ terraform/droplet.tf
@@ -1,3 +1,8 @@
+resource "digitalocean_ssh_key" "mbp" {
+  name       = "mbp"
+  public_key = file("./id_rsa.pub")
+}
+
 # Create a new Web Droplet in the nyc2 region
 resource "digitalocean_droplet" "drone" {
   image  = "ubuntu-20-04-x64"
