1325a94705ca8e86667f7ab1f5e2b839a467e8d3
Brian Adams <service@contrasting.org>
2023-04-25 20:01:40 -0500

    feat: added initial user-data

--- terraform/droplet.tf
+++ terraform/droplet.tf
@@ -11,10 +11,10 @@ resource "digitalocean_droplet" "drone" {
   region   = "nyc3"
   size     = "s-1vcpu-1gb"
   ssh_keys = [digitalocean_ssh_key.mbp.fingerprint]
-  user_data = templatefile("${path.module}/user_data.tmpl.sh"), { 
-    DRONE_GITHUB_CLIENT_ID = var.drone_github_client_id
+  user_data = templatefile("${path.module}/user_data.tmpl.sh", {
+    DRONE_GITHUB_CLIENT_ID     = var.drone_github_client_id
     DRONE_GITHUB_CLIENT_SECRET = var.drone_github_client_secret
-    DRONE_RPC_SECRET=var.drone_rpc_secret
-    DRONE_SERVER_HOST=var.drone_server_host
+    DRONE_RPC_SECRET           = var.drone_rpc_secret
+    DRONE_SERVER_HOST          = var.drone_server_host
   })
 }
