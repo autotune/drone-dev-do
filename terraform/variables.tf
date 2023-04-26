21df0e54e6152987275f20955bcc7ea7804afc80
Brian Adams <service@contrasting.org>
2023-04-25 19:57:06 -0500

    feat: added initial user-data

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "drone_github_client_id" {
+  type = string 
+}
+
+variable "drone_github_client_secret" {
+  type = string 
+}
+
+variable "drone_rpc_secret" {
+  type = string 
+}
+
+variable "drone_server_host" {
+  type = string 
+}
